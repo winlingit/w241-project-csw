1d77e15953db77b6033d941faea950d39eb002dd
Sean Underwood <flyinghamsterster@gmail.com>
2016-04-06 20:24:26 -0500

    More work on randomization and anonymization

--- rcode/randomization.R
+++ rcode/randomization.R
@@ -13,7 +13,7 @@ if(nrow(collectors) != 24) {
 }
 
 assign.treatment <- function(n) {
-  n.treat = floor(n / 2)
+  n.treat = round(n / 2)  # Uses banker's rounding
   n.control = n - n.treat
   data = sample(c(rep(1, n.treat), rep(0, n.control)))
   data
@@ -32,5 +32,19 @@ collectors[, Id := .I]
 dt[, CollectorId := blocknum * 2 - treat]
 merged <- merge(dt, collectors, by.x='CollectorId', by.y='Id')
 
+# Checking results
+# Should have N be at least 2 for all blocks
+# treat.pct should be as close to 50% as is possible given N.
+# I think this is really all we need for checks.
+stats <- unique(merged[, .(.N, Org, Region, Female, treat.pct=mean(treat) ), by=blocknum])
+stats
+
 write.csv(merged, file="Randomized-with-collectors.csv", quote=FALSE, row.names=FALSE)
 
+# Anonymizing by blanking out name and email
+merged[, ':=' (
+  email = '',
+  FirstName = ''
+)]
+
+write.csv(merged, file="De-identified-with-collectors.csv", quote=FALSE, row.names=FALSE)
