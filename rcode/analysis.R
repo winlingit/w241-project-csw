e1240125737165a2ad36ddac87c1b0967825de1c
Sean Underwood <flyinghamsterster@gmail.com>
2016-04-30 21:12:53 -0500

    Working on analysis for final paper.

--- rcode/analysis.R
+++ rcode/analysis.R
@@ -25,9 +25,15 @@ pilot.probs <- genprobexact(pilot.treat) # probability of treatment
 pilot.ate <- estate(pilot.responded, pilot.treat, prob=pilot.probs) # estimate the ATE
 pilot.ate
 
+# Distribution under sharp null
 pilot.Ys <- genouts(pilot.responded, pilot.treat,ate=0) # generate potential outcomes under sharp null of no effect
 pilot.distout <- gendist(pilot.Ys,pilot.perms, prob=pilot.probs) # generate sampling dist. under sharp null
 dispdist(pilot.distout, pilot.ate)  # display characteristics of sampling dist. for inference
+
+# 95% CI
+pilot.Ys <- genouts(pilot.responded, pilot.treat,ate=pilot.ate) ## generate potential outcomes under tau = ATE
+pilot.distout <- gendist(pilot.Ys,pilot.perms, prob=pilot.probs) # generate sampling dist. under tau = ATE
+dispdist(pilot.distout, pilot.ate)  ## display characteristics of sampling dist. for inference
 
 dt.pilot = data.table(treat = pilot.treat, response = pilot.responded)
 dt.pilot = dt.pilot[, .N, by=c('response', 'treat')]
@@ -62,15 +68,12 @@ dt.atg = fread('~/Documents/Berkeley/241/Final\ Project/w241-project-csw/rcode/a
 
 dt.atg[ , .(y = sum(Responses)/sum(N)), by = treat][ , y[1]-y[2]]
 
-dt.atg[, Rate := Responses / N]
-dt.atg[ treat == 1, Treat := 'treatment']
-dt.atg[ treat == 0, Treat := 'control']
 
 # Plot the chart of winning
 ggplot(dt.atg, aes(x=Block, y=Responses, fill=Block)) + 
   geom_bar(stat='identity') +
   geom_text(aes(x=Block, y=Responses, label=Responses), vjust=-.1) +
-  facet_wrap( ~ Treat) +
+  facet_wrap( ~ treat) +
   ggtitle("ATG Responses by Block") + 
   xlab("") + 
   ylab("Responses") + 
@@ -140,10 +143,7 @@ dispdist(distout, ate)  # display characteristics of sampling dist. for inferenc
 
 ## Generate Sampling Distribution Around Estimated ATE
 
-Ys <- genouts(y,Z,ate=ate) ## generate potential outcomes under tau = ATE
-distout <- gendist(Ys,perms, prob=probs) # generate sampling dist. under tau = ATE
-dispdist(distout, ate)  ## display characteristics of sampling dist. for inference
-
+5
 
 ### 2. Analysis with manual RI
 
