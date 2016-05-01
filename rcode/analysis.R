e480d45b58d2563e1953319842c47a59b34d9a75
winlingit <winlin@gmail.com>
2016-05-01 18:35:37 -0500

    deleted extra row for block 2 in PTG results, added column N

--- rcode/analysis.R
+++ rcode/analysis.R
@@ -6,7 +6,7 @@ library(stargazer)
 library(ri)
 
 
-###### ANALYSIS OF ATG EXPERIMENT ######
+###### ATG EXPERIMENT: EXPLORATORY ANALYSIS ######
 
 # read data from remote git repo
 csv = getURL('https://raw.githubusercontent.com/winlingit/w241-project-csw/master/rcode/atg_results.csv')
@@ -53,13 +53,12 @@ ggplot(dt.atg, aes(x=Block, y=Rate, fill=Block)) +
          panel.grid.minor.x=element_blank())
 
 
-
 ###### ATG EXPERIMENT: RANDOMIZATION INFERENCE #######
 
 # recover observations, from: http://stackoverflow.com/questions/2894775/replicate-each-row-of-data-frame-and-specify-the-number-of-replications-for-each
 dt.atgx = dt.atg[rep(seq(.N), N)]  # expand table to 352 rows
-dt.atgx[ , responded := c(rep(1, max(Responses)), rep(0, .N - max(Responses))), by = CollectorId]  # add var for responded
-dt.atg$Responses == dt.atgx[ , sum(responded), by = CollectorId]$V1  # checksums for total responses for each collector
+dt.atgx[ , responded := c(rep(1, max(Responses)), rep(0, .N - max(Responses))), by = CollectorName]  # add var for responded
+dt.atg$Responses == dt.atgx[ , sum(responded), by = CollectorName]$V1  # checksums for total responses for each collector
 
 ### 1. Analysis with "ri" package
 
@@ -122,17 +121,76 @@ p.value = 2*mean(ate.dist >= ATE) # return 2-tailed p-value for estimated ATE
 p.value # p-value < 0.01
 
 
-
 ###### ATG EXPERIMENT: REGRESSION ANALYSIS ######
 
 # regression models
-m1 = lm(responded ~ treat, data = dt.atgx) # treatment only
-m2 = lm(responded ~ treat + Female, data = dt.atgx) # treatment + female
-m3 = lm(responded ~ treat + Female + Org, data = dt.atgx) # treatment + female + org
-m4 = lm(responded ~ treat + Female + Org + Region, data = dt.atgx) # treatment + female + org + region
+m1.atg = lm(responded ~ treat, data = dt.atgx) # treatment only
+m2.atg = lm(responded ~ treat + Female, data = dt.atgx) # treatment + female
+m3.atg = lm(responded ~ treat + Female + Org, data = dt.atgx) # treatment + female + org
+m4.atg = lm(responded ~ treat + Female + Org + Region, data = dt.atgx) # treatment + female + org + region
 
-stargazer(m1, m2, m3, m4, type = 'text', title = 'Regression Analysis for ATG Feedback Survey Experiment')
+stargazer(m1.atg, m2.atg, m3.atg, m4.atg, type = 'text', title = 'Regression Analysis for ATG Feedback Survey Experiment')
+
+
+
+
+###### PTG EXPERIMENT: EXPLORATORY ANALYSIS ######
+
+# read data from remote git repo
+csv = getURL('https://raw.githubusercontent.com/winlingit/w241-project-csw/master/rcode/ptg_results.csv')
+dt.ptg = data.table(read.csv(textConnection(csv)))
+
+# recover observations
+dt.ptgx = dt.ptg[rep(seq(.N), N)]  # expand table to 352 rows
+dt.ptgx[ , responded := c(rep(1, max(Responses)), rep(0, .N - max(Responses))), by = CollectorName]  # add var for responded
+dt.ptg$Responses == dt.ptgx[ , sum(responded), by = CollectorName]$V1  # checksums for total responses for each collector
+
+# estimate overall ATE
+dt.ptg[ , .(y = sum(Responses)/sum(N)), by = treat][ , y[1]-y[2]]
+
+# calculate response rates in each block
+dt.ptg[, Rate := Responses / N]
+dt.ptg[ treat == 1, Treat := 'treatment']
+dt.ptg[ treat == 0, Treat := 'control']
+
+# Plot the chart of winning
+ggplot(dt.ptg, aes(x=block, y=Responses, fill=block)) + 
+        geom_bar(stat='identity') +
+        geom_text(aes(x=block, y=Responses, label=Responses), vjust=-.1) +
+        facet_wrap( ~ treat) +
+        ggtitle("PTG Responses by Block") + 
+        xlab("") + 
+        ylab("Responses") + 
+        theme( axis.line=element_blank(), 
+               axis.text.x=element_blank(),
+               axis.ticks.x=element_blank(),
+               axis.title.x=element_blank(),
+               panel.border=element_blank(),
+               panel.grid.major.x=element_blank(),
+               panel.grid.minor.x=element_blank())
+
+# Plot the chart of proportional winning
+ggplot(dt.ptg, aes(x=block, y=Rate, fill=block)) + 
+        geom_bar(stat='identity') +
+        facet_wrap( ~ Treat) +
+        ggtitle("PTG Response Rates by Block") + 
+        xlab("") + 
+        ylab("Response Rate") + 
+        theme( axis.line=element_blank(), 
+               axis.text.x=element_blank(),
+               axis.ticks.x=element_blank(),
+               axis.title.x=element_blank(),
+               panel.border=element_blank(),
+               panel.grid.major.x=element_blank(),
+               panel.grid.minor.x=element_blank())
 
 
 ###### PTG EXPERIMENT: REGRESSION ANALYSIS ######
 
+# regression models
+m1.ptg = lm(responded ~ treat, data = dt.ptgx) # treatment only
+m2.ptg = lm(responded ~ treat + Female, data = dt.ptgx) # treatment + female
+m3.ptg = lm(responded ~ treat + Female + Org, data = dt.ptgx) # treatment + female + org
+m4.ptg = lm(responded ~ treat + Female + Org + Region, data = dt.ptgx) # treatment + female + org + region
+
+stargazer(m1.ptg, m2.ptg, m3.ptg, m4.ptg, type = 'text', title = 'Regression Analysis for PTG Feedback Survey Experiment')
